37f855103b5c8bc68cd6261d9b3917a06c393621
Emil Wallner <w@llner.co>
2020-12-18 10:27:53 -0800

    Adaptive examiner refactor

--- mingpt/adaptive_examiner.py
+++ mingpt/adaptive_examiner.py
@@ -13,105 +13,115 @@ import random, string
 class AdaptiveExaminer:
     """ Clean data, tokenizer, helper functions """
 
-    def __init__(self, MD):
+    def __init__(self, MD, ac=10, max_batch=358):
         # MemData, Trainer, and Dataset classes
         self.mem_slots = MD.mem_slots
         self.max_trg = MD.max_trg
         self.MD = MD
+        self.max_batch_size = max_batch
+        self.ac = ac
         
         # Batch settings
         self.batch_size = 1
         self.data_lines = 4
-        self.max_batch_size = 358
-        
-        # Adaptive Compute
-        self.ac = 10
-        
+     
     
-    def exam(self, fname, trainer, testing=-1):
+    def exam(self, fname, trainer, test=False, debug=0):
         
-        self.nbr_predictions = testing
+        self.create_filenames()
+        self.test = test
+        self.debug = debug
         self.fname = fname
         self.trainer = trainer
+        self.initiate_at_start()
         
-        # Variables for prediction loop
-        self.prev_src_len = 0
-        self.predict = 0
-        self.batch = 0
-        
-        # Store results
-        self.results = []
-        self.correct_buffer = []
-        self.train_buffer = []
-        
-        train_dataset = MathDataset(fname=fname, MD=self.MD, marker_data=0.0)
-        loader = DataLoader(train_dataset, batch_size=self.batch_size, shuffle=False)
-        dataset_len = len(loader)
-        pbar = tqdm(enumerate(loader), total=dataset_len)
-        x_in = []
-        
-        for batch, (x, y) in pbar:
+        for i in range(self.ac):
+            self.one_loop(i)
             
-            x, x_in = self.concatenate_batches(x, x_in)
+        self.write_file(self, self.train_fn, self.tmp_buffer)
         
-            if self.predict or self.batch == self.max_batch_size or batch+1 == dataset_len:
-                pred, clip_src = self.make_prediction(x_in)
-                for i in range(x_in.size(0)):
-                    src_trg_pred_mem = self.extract_src_trg_pred_mem(x_in[i], pred[i], clip_src)
-                    self.log_results(src_trg_pred_mem)
-            
-            # report progress
-            pbar.set_description(f"Iiter {batch}")
-            if self.nbr_predictions >= 0 and batch+1 >= self.nbr_predictions:
-                break
+        if os.path.exists(self.tmp_fn):
+            os.remove(self.tmp_fn)
         
         results = self.results
+        preds = self.predictions
         print("Final score: %d/%d = %.2f%% correct" % (np.sum(results), len(results), 100*np.mean(results)))
-        print("Saving new files to disk...")
+        print("Final predictions: %d/%d = %.2f%% correct" % (np.sum(preds), len(preds), 100*np.mean(preds)))
+        
+    def one_loop(self, current_it):
+        
+        self.initiate_vars()
+        fname == self.train_fn if current_it == 0 else self.tmp_fn
+        dataset = MathDataset(fname=fname, MD=self.MD, marker_data=0.0)
+        loader = DataLoader(dataset, batch_size=self.batch_size, shuffle=False)
+        if current_it == 0: self.initiate_at_start()
+        
+        dataset_len = len(loader)
+        pbar = tqdm(enumerate(loader), total=dataset_len)
+        batch = []
+        
+        for b, (x, y) in pbar:
+            
+            batch = self.concatenate_batches(batch, x)
+            if b+1 == dataset_len: self.predict = 1
+        
+            if self.predict:
+                output = self.model_predict(batch)
+                cut_input = self.get_input_len(batch[0]) + 1
+                batch_sz = batch.size(0)
+                
+                for i in range(batch_sz):
+                    clean_output = self.clean_output(batch[i], output[i], cut_input)
+                    self.log_results(clean_output)
+                
+                # Log status
+                pbar.set_description(f"Iiter {b} Score: {np.sum(self.results)}/{len(self.results)}")
+            
+            if self.debug and b+1 >= self.debug:
+                break
+        
         self.save_result_to_file()
                     
 
-    def concatenate_batches(self, x, x_in):
+    def concatenate_batches(self, batch, x):
         
-        clip_src_mem = self.get_clip_src_mem_len(x[0]) 
-        x_in = leftover if self.prev_src_len == -1 else x_in
+        new_len = self.get_input_len(x[0]) 
+        batch = self.leftover if self.buffer == True else batch
 
         # Concat input source with same length
-        if self.prev_src_len == clip_src_mem:
-            x_in = torch.cat((x_in, x), 0)
-        elif self.prev_src_len == 0:
-            x_in = x
-        else:
-            self.prev_src_len = -1
-            self.predict = 1
-            leftover = x
-        self.prev_src_len = clip_src_mem
-        self.batch += 1
+        if self.len == new_len: batch = torch.cat((batch, x), 0)
+        elif self.buffer == False: batch = x
+        else: self.predict, self.leftover = 1, x
+   
+        self.len = new_len
+        self.batch_size += 1
+        if self.batch_size == self.max_batch_size: self.predict = 1
         
-        return x, x_in
+        return batch
         
-    def make_prediction(self, x_in):
+    def model_predict(self, x_in):
         
         # Reset tracker variables
-        self.batch, self.predict, self.prev_src_len, = 0, 0, 0
-        clip_src_mem = self.get_clip_src_mem_len(x_in[0]) + 1 
-        x_cut = x_in[:, :clip_src_mem]
-
+        self.batch, self.predict, self.len, self.buffer = 0, 0, 0, False
+        
+        # Prepare input
+        cut_index = self.get_input_len(x_in[0]) + 1 
+        x_cut = x_in[:, :cut_index]
         pred = x_cut.to(self.trainer.device)
-        pred = sample(self.trainer.model, pred, int(self.max_trg+1))
         
-        return pred, clip_src_mem
+        # Make prediction
+        output = sample(self.trainer.model, pred, int(self.max_trg+1))
+        
+        return output
     
-    def get_clip_src_mem_len(self, x):
+    def get_input_len(self, x):
         
-        if self.mem_slots:
-            clip_src_mem = self.MD.locate_token('mem-end', x)
-        else:
-            clip_src_mem = self.MD.locate_token('answer', x) 
+        if self.mem_slots: clip_src_mem = self.MD.locate_token('mem-end', x)
+        else: clip_src_mem = self.MD.locate_token('answer', x) 
         
         return clip_src_mem
     
-    def extract_src_trg_pred_mem(self, x, pred, cut_src_mem):
+    def clean_output(self, x, pred, cut_input):
         
         # Extract src from data
         cut_src = self.MD.locate_token('answer', x)
@@ -119,11 +129,12 @@ class AdaptiveExaminer:
         
         # Extract trg from data
         cut_padding = self.MD.locate_token('pad', x)
-        trg = x[cut_src_mem:cut_padding] # X does not have the 'finish' token
+        trg = x[cut_input:cut_padding] # X does not have the 'finish' token
         
         # Extract prediction from data
         cut_pred = self.MD.locate_token('finish', pred)
-        pred = pred[cut_src_mem:cut_pred]
+        mark = self.MD.idx[pred[cut_pred+1].tolist()]
+        pred = pred[cut_input:cut_pred]
         
         # Translate the tensors to strings
         src = self.MD.tensor2string(src)
@@ -133,11 +144,11 @@ class AdaptiveExaminer:
         # Extract memory from data
         mem = []
         if self.mem_slots:
-            mem = self.tensor_to_mem_slots(x[cut_src+1:], pred)
+            mem = self.tensor2memory(x[cut_src+1:], pred)
         
-        return [src] + [trg] + [pred] + mem
+        return [src] + [trg] + [pred] + [mark] + mem
     
-    def tensor_to_mem_slots(self, x, pred):
+    def tensor2memory(self, x, pred):
         
         mem = []
      
@@ -152,39 +163,42 @@ class AdaptiveExaminer:
         return mem
 
             
-    def log_results(self, src_trg_pred_mem):
+    def log_results(self, output):
         
-        correct = 1 if src_trg_pred_mem[1] == src_trg_pred_mem[2] else 0
-        self.results.append(correct)
-        if correct:
-            self.correct_buffer.append(src_trg_pred_mem)
+        # Output = [src] + [trg] + [pred] + [mark] + mem
+        correct_p = output[3] == 'right'
+        correct_r = 1 if output[1] == output[2] else 0
+        correct_pr = int(correct_p and correct_r)
+        prediction_score = int(correct_p == correct_r)
+        # TODO: Seperate local and global results
+        self.results.append(correct_pr)
+        self.predictions.append(prediction_score)
+        
+        # Create training data
+        if correct_pr:
+            self.correct_buffer.append(output)
+        elif (correct_p and not correct_r) or (not correct_p and correct_r):
+            output[3] = f"Predicted: {correct_p}, Result: {correct_r}"
+            self.train_buffer.append(output)
         else:
-            self.train_buffer.append(src_trg_pred_mem)
+            self.temp_buffer.append(output)
 
     def save_result_to_file(self):
         
-        head_tail = os.path.split(self.fname)
-        correct_fname = head_tail[0] + '/correct_' + head_tail[1] + datetime.datetime.now().strftime('%Y-%m-%d~%H:%M:%S')
-        train_fname = head_tail[0] + '/' + head_tail[1]
+        self.write_file(self, self.train_fn, self.train_buffer)
+        self.write_file(self, self.tmp_fn, self.tmp_buffer)
+        self.write_file(self, self.correct_fn, self.correct_buffer)
         
-        if os.path.exists(correct_fname):
-            os.remove(correct_fname)
-        if os.path.exists(train_fname):
-            os.remove(train_fname)
+    def write_file(self, fname, buffer):
         
-        with open(correct_fname, "a") as file:
-            indexes = self.sort_data_by_srcmem_len(self.correct_buffer)
+        with open(fname, "a") as file:
+            indexes = self.sort_data_len(buffer)
             for index in indexes:
-                for item in self.correct_buffer[index]:
+                for item in buffer[index]:
                     file.write(item + '\n')
         
-        with open(train_fname, "a") as file:
-            indexes = self.sort_data_by_srcmem_len(self.train_buffer)
-            for index in indexes:
-                for item in self.train_buffer[index]:
-                    file.write(item + '\n')
         
-    def sort_data_by_srcmem_len(self, data):
+    def sort_data_len(self, data):
         indexes = list(range(len(data)))
         sorted_data = []
         for index in indexes:
@@ -197,3 +211,18 @@ class AdaptiveExaminer:
         
         sorted_data = sorted(sorted_data, key=lambda x: x[1])
         return [i[0] for i in sorted_data]
+    
+    def create_filenames(self):
+        head_tail = os.path.split(self.fname)
+        self.train_fn = head_tail[0] + '/' + head_tail[1]
+        self.correct_fn = head_tail[0] + '/correct_' + head_tail[1]
+        self.temp_fn = head_tail[0] + '/temp_' + head_tail[1]
+    
+    def initiate_vars(self):
+        self.tmp_buffer, self.train_buffer, self.correct_buffer = [], [], []
+        self.len, self.predict, self.batch, self.buffer = 0, 0, 0, False
+        
+    def initiate_at_start(self):
+        self.results, self.predictions = [], []
+        if os.path.exists(train_fname):
+            os.remove(train_fname)
